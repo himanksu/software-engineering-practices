7a916596c590310d134f2c7582d521bcc8636e83
Zain <zainullahdev@outlook.com>
2020-04-30 17:47:44 +0400

    feat: added gausian package

---
+++ OOP/gaussian package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+     name='distributions',  
+     version='1.1',
+     author="Zain Ullah Muhammad Idrees",
+     author_email="zainullahdev@outlook.com",
+     description="Gaussian Model that includes base functions"
+ )
